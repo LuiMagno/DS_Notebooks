72bd70b257495bead8de6584c8c96bae2f153844
Lui Magno Costa Araújo <luicosta3@gmail.com>
2023-06-06 10:36:58 -0300

    Exercícios iniciais

--- FTC - Python/test.py
+++ FTC - Python/test.py
@@ -1,6 +1,37 @@
-nums = []
-for x in range(8):
-    nums.append(x);
+# numero01 = input()
+# numero02 = input()
 
-print(len(nums));
-print(nums);
+# soma = numero01 + numero02
+
+# print("O resultado da soma é: ", soma)
+
+# nome = input()
+
+# if nome == "pedro":
+#     print("Bem-vindo Pedro")
+# else:
+#     print("Bem-vindo Meigarom")
+
+# valor = int(input())
+# resultado = 0;
+# if valor > 10:
+#     resultado = valor * 100
+# else:
+#     resultado = valor + 10
+
+# print("O resultado é igual a: ", resultado)
+
+numero1 = int(input())
+numero2 = int(input())
+numero3 = int(input())
+
+if numero1 > numero2:
+    if numero1 > numero3:
+        print("Numero 1: ", numero1, " é o maior.")
+    else:
+        print("Numero 3: ", numero3, " é o maior.")
+elif numero2 > numero3:
+    print("Numero 2: ", numero2, " é o maior.")
+else:
+    print("Numero 3: ", numero3, " é o maior.")
+
